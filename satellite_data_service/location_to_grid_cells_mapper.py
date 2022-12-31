c5d2bd9487b59b7ec38f02787a2ec60d20e76d5f
Erik Wessel <erik.wessel@student.kit.edu>
2022-12-31 17:25:45 +0100

    make downloads automatic and add config.yml
    - replace download_grid.sh script with dynamic behavior in python
    - add config YAML file to make filepath and download-url changeable

--- satellite_data_service/location_to_grid_cells_mapper.py
+++ satellite_data_service/location_to_grid_cells_mapper.py
@@ -1,8 +1,11 @@
 import logging
+import os
 from typing import Dict, List, Tuple
 
 import fiona
 import geopandas as gpd
+import requests
+import yaml
 
 MappingType = Dict[Tuple[float, float], List[str]]
 """This type denotes a dict of (longitude, latitude) tuples for keys with a list of grid-cell-names"""
@@ -10,20 +13,28 @@ MappingType = Dict[Tuple[float, float], List[str]]
 class LocationToGridCellsMapper ():
     """Provides mappings from location-points to the grid-cells in which they are located"""
 
-    __grid: gpd.GeoDataFrame
-    """
-    The grid that is composed of polygons, which in turn cover a certain area of the planet.
-    Instead of using this variable directly, use the `get_grid()` method to make sure it is properly initialized.
-    """
-
     def __init__(self) -> None:
         self.__grid = None
+        """
+        The grid that is composed of polygons, which in turn cover a certain area of the planet.
+        Instead of using this variable directly, use the `get_grid()` method to make sure it is properly initialized.
+        """
+        config = yaml.safe_load(open('config.yml'))
+        self.grid_filepath = config['grid']['filepath']
+        self.grid_download_url = config['grid']['download_url']
 
     def load_grid(self):
         """Provides a way to initialize and reload the grid"""
         logging.info('Loading Sentinel-Grid..')
+        if not os.path.exists(self.grid_filepath):
+            logging.info('Sentinel-Grid is not avaiable, downloading..')
+            response = requests.get(self.__grid_download_url)
+            response.raise_for_status()
+            with open(self.grid_filepath, 'wb') as file:
+                file.write(response.content)
+            logging.info('Sentinel-Grid download complete!')
         fiona.supported_drivers['KML'] = 'rw' # enable KML support
-        self.__grid = gpd.read_file('data/sentinel_2_level_1c_tiling_grid.kml', driver='KML')
+        self.__grid = gpd.read_file(self.grid_filepath, driver='KML')
         logging.info('Sentinel-Grid loaded!')
 
     def get_grid(self) -> gpd.GeoDataFrame:
