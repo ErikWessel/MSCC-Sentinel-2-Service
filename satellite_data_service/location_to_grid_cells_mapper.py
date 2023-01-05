3d4f47c56becc4b9658777a59933c8c5628c1fea
Erik Wessel <erik.wessel@student.kit.edu>
2023-01-05 17:00:04 +0100

    static grid data and updated data path

--- satellite_data_service/location_to_grid_cells_mapper.py
+++ satellite_data_service/location_to_grid_cells_mapper.py
@@ -13,28 +13,29 @@ MappingType = Dict[Tuple[float, float], List[str]]
 class LocationToGridCellsMapper ():
     """Provides mappings from location-points to the grid-cells in which they are located"""
 
+    __grid = None
+    """
+    The grid that is composed of polygons, which in turn cover a certain area of the planet.
+    Instead of using this variable directly, use the `get_grid()` method to make sure it is properly initialized.
+    """
+
     def __init__(self) -> None:
-        self.__grid = None
-        """
-        The grid that is composed of polygons, which in turn cover a certain area of the planet.
-        Instead of using this variable directly, use the `get_grid()` method to make sure it is properly initialized.
-        """
         config = yaml.safe_load(open('config.yml'))
         self.grid_filepath = config['grid']['filepath']
-        self.grid_download_url = config['grid']['download_url']
+        self.grid_download_url = config['grid']['download-url']
 
     def load_grid(self):
         """Provides a way to initialize and reload the grid"""
         logging.info('Loading Sentinel-Grid..')
         if not os.path.exists(self.grid_filepath):
             logging.info('Sentinel-Grid is not avaiable, downloading..')
-            response = requests.get(self.__grid_download_url)
+            response = requests.get(self.grid_download_url)
             response.raise_for_status()
             with open(self.grid_filepath, 'wb') as file:
                 file.write(response.content)
             logging.info('Sentinel-Grid download complete!')
         fiona.supported_drivers['KML'] = 'rw' # enable KML support
-        self.__grid = gpd.read_file(self.grid_filepath, driver='KML')
+        LocationToGridCellsMapper.__grid = gpd.read_file(self.grid_filepath, driver='KML')
         logging.info('Sentinel-Grid loaded!')
 
     def get_grid(self) -> gpd.GeoDataFrame:
@@ -46,9 +47,9 @@ class LocationToGridCellsMapper ():
         `geopandas.GeoDataFrame`
             The grid that is composed of polygons, which in turn cover a certain area of the planet
         """
-        if self.__grid is None:
+        if LocationToGridCellsMapper.__grid is None:
             self.load_grid()
-        return self.__grid
+        return LocationToGridCellsMapper.__grid
 
     def selectLocationContainingGridCells(self, locations:gpd.GeoDataFrame) -> gpd.GeoDataFrame:
         """
